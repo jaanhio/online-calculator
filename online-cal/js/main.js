0c9c772e92cfb13060647c53b99486cf1369a027
Jianhao Tan <jianhaot.89@gmail.com>
2017-06-06 08:26:26 +0800

    inclusion of jquery functions

--- online-cal/js/main.js
+++ online-cal/js/main.js
@@ -1,11 +1,29 @@
+
+var histArr=[];
+var histStr;
+
 $(document).ready(function(){
 
   $('#1').on('click', function(e){
-    $('#input').html("2");
-    $('#hist').html("2");
+    histArr.push("1");
+    $('#input').html("1");
+    histStr=histArr.join("");
+    $('#hist').html(histStr);
   });
 
+  $('#2').on('click', function(e){
+    histArr.push("2");
+    $('#input').html("2");
+    histStr=histArr.join("");
+    $('#hist').html(histStr);
+  });
 
+  $('#addition').on('click', function(e){
+    histArr.push("+");
+    $('#input').html("+");
+    histStr=histArr.join("");
+    $('#hist').html(histStr);
+  });
 
 
 });
